8850254e1b5f424ac55e6b6b3c04a31e7f9c5017
David Maxson <david.maxson@rearc.io>
2023-04-19 12:51:13 -0700

    Bump version: 0.0.1 → 0.0.2

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "APInator"
 copyright = "2023, Rearc Data"
 author = "Rearc Data"
-release = "0.0.1"
+release = "0.0.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
